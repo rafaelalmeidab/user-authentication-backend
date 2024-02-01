f8cd862a451ab47d99ffb278f713b68424c15d30
rafaelalmeidab <rafaelalmeidab@hotmail.com>
2024-01-31 22:47:04 -0300

    feature: rota listar e buscar usuarios

--- projeto-autenticacao/app.js
+++ projeto-autenticacao/app.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
 var mysql = require('mysql');
+
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -7,10 +13,34 @@ var con = mysql.createConnection({
   database: "barmanagementdb"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM usuarios", function (err, result, fields) {
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
+// Rota de login (página de login)
+app.get('/login/users', (req, res) => {
+  con.connect(function(err) {
     if (err) throw err;
-    console.log(result);
+    con.query("SELECT ID, NOME FROM usuarios", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+    });
+  });
+});
+
+app.post('/login/users', (req, res) => {
+  let user = req.body.user; 
+  const sql = "SELECT ID, NOME FROM usuarios WHERE NOME = '" + user + "'";
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query(sql, function (err, result, fields) {
+      // if (err) throw err;
+      if(result){res.send(result);}
+      else{res.send("Usuário não cadastrado!")}
+    });
   });
 });
