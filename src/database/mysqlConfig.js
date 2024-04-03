7eb3abf61c8155bd9cf3e365aa624821d19fb595
Rafael Almeida <74375870+rafaelalmeidab@users.noreply.github.com>
2024-04-03 17:45:59 -0300

    Update mysqlConfig.js

--- src/database/mysqlConfig.js
+++ src/database/mysqlConfig.js
@@ -3,7 +3,7 @@ const config = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'barmanagementdb',
+        database: 'db',
         port: 3306,
         connectionLimit: 10,
         connectTimeout: 60000
