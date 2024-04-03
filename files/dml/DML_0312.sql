6429101441ef29748e5887f7239f82d3d1b33c62
Rafael Almeida <74375870+rafaelalmeidab@users.noreply.github.com>
2024-04-03 17:45:03 -0300

    Update DML_0312.sql

--- files/dml/DML_0312.sql
+++ files/dml/DML_0312.sql
@@ -1,3 +1,3 @@
-INSERT INTO barmanagementdb.usuarios (NOME,EMAIL,SENHA,DTHRINSERT,DTHRUPDATE) VALUES
-	 ('admin','rafaelalmeidab@hotmail.com','81dc9bdb52d04dc20036dbd8313ed055','2024-01-30 21:47:48','2024-01-30 21:47:48'),
-	 ('admin2','rafaelalmeidab@hotmaill.com','8c4fa212f83ab7e8ff25b03b846af92c','2024-01-30 21:48:25','2024-01-30 21:48:25');
+INSERT INTO db.usuarios (NOME,EMAIL,SENHA,DTHRINSERT,DTHRUPDATE) VALUES
+	 ('admin','a@hotmail.com','81dc9bdb52d04dc20036dbd8313ed055','2024-01-30 21:47:48','2024-01-30 21:47:48'),
+	 ('admin2','b@hotmaill.com','8c4fa212f83ab7e8ff25b03b846af92c','2024-01-30 21:48:25','2024-01-30 21:48:25');
